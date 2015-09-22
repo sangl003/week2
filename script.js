6f17e6d0728cbd5b89c4c7f04036f0c65c050321
Sharvari <sangl003@umn.edu>
2015-09-22 23:04:57 +0000

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 
 
 		var map = L.map('map-container');		
-		map.setView([44.971724, -93.243239], 18);		
+		map.setView([44.971724, -93.243239], 16);		
 		L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
 		maxZoom: 18,
 		}).addTo(map);
